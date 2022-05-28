27590af0aa12de0b65ee63d5adfa554a7ceea7ff
zeWhiteBeats <106404043+zeWhiteBeats@users.noreply.github.com>
2022-05-28 20:00:33 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,35 @@
-document.querySelector("#input").addEventListener("keydown", (event) => {
-  if(event.key === "Enter"){
+
+document.querySelector("#input").addEventListener("keydown", function(event) {
+    if(event.key === "Enter"){
+      const input = document.querySelector("#input");
+      addItem(input.value);
+      console.log(input.value);
+    }}
+);
+  
+  document.querySelector("#add_item").addEventListener("click", function(){
     const input = document.querySelector("#input");
     addItem(input.value);
+  });
+    function text(form){
+      if(strlen(form.searc.value)>0){
+        alert("длина текста не совпадает");
+        return false;
+      }
+      else{
+        return true;
+      }
   }
     
-}};
-
-document.querySelector("#add_item").addEventListener("click", () => {
-  const input = document.querySelector("#input");
-  addItem(input.value);
-}};
-  function text(form){
-    if(strlen(form.searc.value)>0){
-      alert("длина текста не совпадает");
-      return false;
-    }
-    else{
-      return true;
-    }
-}
-  
-function addItem(input) => {
-  const item = document.createElement("div");
-  const div = document.createElement("div");
-  const checkIcon = document.createElement("i");
-  const trashIcon = document.createElement("i");
-  const text = document.createElement("p");
-}
+  function addItem(input) {
+    const item = document.createElement("div");
+    const div = document.createElement("div");
+    const checkIcon = document.createElement("i");
+    const trashIcon = document.createElement("i");
+    const text = document.createElement("p");
   item.className = "item";
-  text.textContent = input;
+    text.textContent = input;
+  document.body.append(text)
+  
 
-
-  checkIcon.className = "fas fa-check-square";
-  checkIcon.style.color = "lightgray";
-  checkIcon.addEventListener("click", () => {
-    checkIcon.style.color = "limegreen";
-  )
-  div.appendChild(checkIcon);
-
-  trashIcon.className = "fas fa-trash";
-  trashIcon.style.color = "darkgray";
-  trashIcon.addEventListener("click", () => {
-    item.remove();
-    }
-  div.appendChild(trashIcon);
-
-  item.appendChild(text);
-  item.appendChild(div);
-  document.querySelector("#input").value = "";
-}
+  };
